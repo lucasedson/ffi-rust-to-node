8f9d846d39ee761d3e55c5bf32d297ef008d996f
Lucas Edson <33698741+lucasedson@users.noreply.github.com>
2023-09-15 15:17:47 +0000

    ok

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const ffi = require('ffi-napi')
 
 var lib = ffi.Library("simple-ffi/target/release/libsimple_ffi", {
-    'my_sum' : ['int', ['int', 'int'] ]
+    'my_sum' : ['int', ['int', 'int'] ],
+    'my_minus' : ['int', ['int', 'int'] ]
 })
 
-const {my_sum} = lib
+const {my_sum, my_minus} = lib
 
 let soma = my_sum(150,14)
 
+let menos = my_minus(250, 48)
+
 console.log(soma);
+console.log(menos)
--- simple-ffi/src/lib.rs
+++ simple-ffi/src/lib.rs
@@ -2,3 +2,8 @@
 pub extern "C" fn my_sum(a: i32, b: i32) -> i32 {
     a + b
 }
+
+#[no_mangle]
+pub extern "C" fn my_minus(a: i32, b: i32) -> i32 {
+    return a - b;
+}
